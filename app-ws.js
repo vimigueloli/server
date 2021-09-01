7c54123fe82478a746ea9a575f672e040d52572a
vimigueloli <vipeso2016@gmail.com>
2021-08-31 21:52:08 -0300

    backend do projeto restaurante

---
+++ app-ws.js
@@ -0,0 +1,59 @@
+const WebSocket = require('ws');
+
+
+
+function onError(ws, err) {
+    console.error(`onError: ${err.message}`);
+}
+
+function onMessage(ws, data) {
+    console.log(`onMessage: ${data}`);
+    ws.send(`${data}`);
+    broadcast({data:data})
+    return(data)
+}
+
+function onConnection(ws, req) {
+    ws.on('message', data => onMessage(ws, data));
+    ws.on('error', error => onError(ws, error));
+    console.log(`onConnection`);
+}
+
+function broadcast(jsonObject) {
+    if (!this.clients) return;
+    this.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(jsonObject));
+        }
+    });
+}
+
+function corsValidation(origin) {
+    return process.env.CORS_ORIGIN === '*' || process.env.CORS_ORIGIN.startsWith(origin);
+}
+
+function verifyClient(info, callback) {
+    if (!corsValidation(info.origin)) return callback(false);
+
+    const token = info.req.url.split('token=')[1];
+
+    if (token) {
+        if (token === '123456')
+            return callback(true);
+    }
+
+    return callback(false);
+}
+
+module.exports = (server) => {
+    const wss = new WebSocket.Server({
+        server,
+        verifyClient
+    });
+
+    wss.on('connection', onConnection);
+    wss.broadcast = broadcast;
+
+    console.log(`App Web Socket Server is running!`);
+    return wss;
+}
---
+++ index.js
@@ -0,0 +1,18 @@
+const WebSocket = require('ws');
+const server = new WebSocket.Server({
+  port: 3001
+});
+let sockets = [];
+server.on('connection', function(socket) {
+  // Adicionamos cada nova conexão/socket ao array `sockets`
+  sockets.push(socket);
+  // Quando você receber uma mensagem, enviamos ela para todos os sockets
+  socket.on('message', function(msg) {
+    sockets.forEach(s => s.send(msg));
+  });
+  // Quando a conexão de um socket é fechada/disconectada, removemos o socket do array
+  socket.on('close', function() {
+    sockets = sockets.filter(s => s !== socket);
+  });
+});
+
